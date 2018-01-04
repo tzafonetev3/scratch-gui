123399159cbcd97a39b24a90adb9ac7b577aff47
Kreg Hanning <khanning@gmail.com>
2018-01-04 15:58:28 -0500

    Build for 69c0699bd4d07a3c8f62b9bd754de746594bf3f0

--- gui.js
+++ gui.js
@@ -1,6 +1,6 @@
 webpackJsonp([0],{
 
-/***/ 125:
+/***/ 130:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -16,23 +16,23 @@ var _reactDom = __webpack_require__(12);
 
 var _reactDom2 = _interopRequireDefault(_reactDom);
 
-var _reactModal = __webpack_require__(46);
+var _reactModal = __webpack_require__(18);
 
 var _reactModal2 = _interopRequireDefault(_reactModal);
 
-var _appStateHoc = __webpack_require__(23);
+var _appStateHoc = __webpack_require__(25);
 
 var _appStateHoc2 = _interopRequireDefault(_appStateHoc);
 
-var _gui = __webpack_require__(24);
+var _gui = __webpack_require__(26);
 
 var _gui2 = _interopRequireDefault(_gui);
 
-var _projectLoaderHoc = __webpack_require__(28);
+var _projectLoaderHoc = __webpack_require__(31);
 
 var _projectLoaderHoc2 = _interopRequireDefault(_projectLoaderHoc);
 
-var _index = __webpack_require__(425);
+var _index = __webpack_require__(467);
 
 var _index2 = _interopRequireDefault(_index);
 
@@ -57,13 +57,13 @@ _reactDom2.default.render(_react2.default.createElement(App, null), appTarget);
 
 /***/ }),
 
-/***/ 425:
+/***/ 467:
 /***/ (function(module, exports, __webpack_require__) {
 
 // style-loader: Adds some css to the DOM by adding a <style> tag
 
 // load the styles
-var content = __webpack_require__(426);
+var content = __webpack_require__(468);
 if(typeof content === 'string') content = [[module.i, content, '']];
 // Prepare cssTransformation
 var transform;
@@ -89,7 +89,7 @@ if(false) {
 
 /***/ }),
 
-/***/ 426:
+/***/ 468:
 /***/ (function(module, exports, __webpack_require__) {
 
 exports = module.exports = __webpack_require__(2)(undefined);
@@ -106,5 +106,5 @@ exports.locals = {
 
 /***/ })
 
-},[125]);
+},[130]);
 //# sourceMappingURL=gui.js.map
