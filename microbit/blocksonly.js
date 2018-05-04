75ac1b907c81a73cc12bda4b33e35b51bfaff0ea
khanning <khanning@khanningosx.media.mit.edu>
2018-05-04 11:00:21 -0400

    "Build

--- microbit/blocksonly.js
+++ microbit/blocksonly.js
@@ -1,7 +1,7 @@
 var GUI =
 webpackJsonpGUI([2],{
 
-/***/ 575:
+/***/ 569:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -17,23 +17,23 @@ var _reactDom2 = _interopRequireDefault(_reactDom);
 
 var _reactRedux = __webpack_require__(8);
 
-var _controls = __webpack_require__(55);
+var _controls = __webpack_require__(52);
 
 var _controls2 = _interopRequireDefault(_controls);
 
-var _blocks = __webpack_require__(103);
+var _blocks = __webpack_require__(99);
 
 var _blocks2 = _interopRequireDefault(_blocks);
 
-var _gui = __webpack_require__(25);
+var _gui = __webpack_require__(23);
 
 var _gui2 = _interopRequireDefault(_gui);
 
-var _projectLoaderHoc = __webpack_require__(42);
+var _projectLoaderHoc = __webpack_require__(39);
 
 var _projectLoaderHoc2 = _interopRequireDefault(_projectLoaderHoc);
 
-var _blocksOnly = __webpack_require__(576);
+var _blocksOnly = __webpack_require__(570);
 
 var _blocksOnly2 = _interopRequireDefault(_blocksOnly);
 
@@ -69,11 +69,11 @@ _reactDom2.default.render(_react2.default.createElement(App, null), appTarget);
 
 /***/ }),
 
-/***/ 576:
+/***/ 570:
 /***/ (function(module, exports, __webpack_require__) {
 
 
-var content = __webpack_require__(577);
+var content = __webpack_require__(571);
 
 if(typeof content === 'string') content = [[module.i, content, '']];
 
@@ -120,7 +120,7 @@ if(false) {
 
 /***/ }),
 
-/***/ 577:
+/***/ 571:
 /***/ (function(module, exports, __webpack_require__) {
 
 exports = module.exports = __webpack_require__(2)(false);
@@ -137,5 +137,5 @@ exports.locals = {
 
 /***/ })
 
-},[575]);
+},[569]);
 //# sourceMappingURL=blocksonly.js.map
